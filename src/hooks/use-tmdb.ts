378114e027d15a1e987e54c8f07a7fc4cc1164d7
Monkiey <meaomeao007@gmail.com>
2026-07-23 14:01:08 -0700

    fix: use NEXT_PUBLIC_TMDB_TOKEN in useTmdb hook

--- src/hooks/use-tmdb.ts
+++ src/hooks/use-tmdb.ts
@@ -3,6 +3,8 @@
 import { createContext, useContext } from 'react';
 import { TMDB } from '@lorenzopant/tmdb';
 
+import { env } from '@/env.mjs';
+
 type TmdbContextType = { tmdb: TMDB };
 
 export const TmdbContext = createContext<TmdbContextType | null>(null);
@@ -10,11 +12,9 @@ export const TmdbContext = createContext<TmdbContextType | null>(null);
 export function useTmdb() {
   const context = useContext(TmdbContext);
   if (!context) {
-    // Return a default TMDB instance with environment or fallback key if provider not wrapped
-    const apiKey =
-      process.env.NEXT_PUBLIC_TMDB_API_KEY ??
-      '4f4b279ca355d27d9266ddc9944f404c';
-    return new TMDB(apiKey);
+    const token =
+      env.NEXT_PUBLIC_TMDB_TOKEN ?? process.env.NEXT_PUBLIC_TMDB_TOKEN ?? '';
+    return new TMDB(token);
   }
   return context.tmdb;
 }
